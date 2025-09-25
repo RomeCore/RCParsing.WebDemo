0f73052bcd2352738aad584ae5388e586b5cea39
RomeCore <62770895+RomeCore@users.noreply.github.com>
2025-09-25 18:18:13 +0500

    Initial commit

---
+++ RCParsing.WebDemo/Pages/Home.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace RCParsing.WebDemo.Pages
+{
+	public partial class Home
+	{
+		private string gv =
+		"""
+		flowchart TD
+			Root["- "] --> Multiply["*"]
+			Root --> VariableC["c"]
+
+			Multiply --> VariableA["a"]
+			Multiply --> Plus["+"]
+
+			Plus --> VariableB["b"]
+			Plus --> Literal5["5"]
+		""";
+	}
+}
