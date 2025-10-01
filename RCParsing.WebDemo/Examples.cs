5492de8b3adfccf7670717963f1d51fbcfbc6d06
RomeCore <62770895+RomeCore@users.noreply.github.com>
2025-10-01 21:32:12 +0500

    Add examples selection

---
+++ RCParsing.WebDemo/Examples.cs
@@ -0,0 +1,76 @@
+﻿namespace RCParsing.WebDemo
+{
+	public static class Examples
+	{
+		public static readonly Dictionary<string, (string, string)> examples;
+
+		static Examples()
+		{
+			examples = new Dictionary<string, (string, string)>
+			{
+				["JSON"] = (
+				"""
+				$skip : WS ;
+				$main : value ;
+
+				NUMBER : /\d+(?:\.\d+)?/ ;
+				STRING : /"[^"]*"/ ;
+				BOOL : 'true' | 'false' ;
+				NULL : 'null' ;
+
+				pair : STRING ':' value ;
+				array : '[' value % ',' * ']' ;
+				object : '{' pair % ',' * '}' ;
+				value : NUMBER | STRING | BOOL | NULL | array | object ;
+				"""
+				,
+				"""
+				{
+					"name": "John Doe",
+					"age": 21,
+					"tags": [ "tag1", "tag2", "tag3" ]
+				}
+				"""
+				),
+
+				["Math expressions"] = (
+				"""
+				$skip : WS ;
+				$main : expression ;
+
+				NUMBER : /\d+/ ;
+				ADD_OP : '+' | '-' ;
+				MUL_OP : '*' | '/' ;
+
+				expression : term % ADD_OP + ;
+				term : factor % MUL_OP + ;
+				factor : NUMBER | '(' expression ')' ;
+				"""
+				,
+				"""
+				2 + 3 * (4 - 1)
+				"""
+				),
+
+				["Simple CSV"] = (
+				"""
+				$skip : SPACES ;
+				$main : csv ;
+
+				STRING : /[^,\n\r]+/ ;
+				
+				row : STRING % ',' + ;
+				csv : row % NEWLINE + ;
+				"""
+				,
+				"""
+				Name,Age,City
+				John,25,New York
+				Alice,30,London
+				Bob,22,Paris
+				"""
+				)
+			};
+		}
+	}
+}
